e166cf64eb93d391590d7a49e0f65ac427540ee3
maginist <maginist@student.42.fr>
2021-01-11 16:55:15 +0100

    add write_theta

--- train.py
+++ train.py
@@ -8,7 +8,7 @@ import matplotlib.pyplot as plt
 
 class Process(object):
 
-    def __init__(self, datafile, output, rate=0.1, range=1000):
+    def __init__(self, datafile, output, rate, range):
         self.output = output
         self.theta = [0, 0]
         self.range = range
@@ -18,12 +18,34 @@ class Process(object):
         self.price_ref = self.data[:, 1]
         self.km = self.standardize(self.data[:, 0])
         self.price = self.standardize(self.data[:, 1])
+        self.sum_km = np.sum(self.data[:,0])
+        self.sum_price = np.sum(self.data[:,1])
 
     def standardize(self, x):
         return ((x - np.mean(x)) / np.std(x))
 
     def destandardize(self, x, x_ref):
         return x * np.std(x_ref) + np.mean(x_ref)
+
+    def mean_square_error(self):
+        return
+
+    def gradient_descent(self):
+        return
+
+    def write_theta(self):
+        try:
+            f = open(training.output, "w")
+            print("coucou")
+            f.write(f"theta0,theta1\n%d,%d"% training.theta[:,0], training.theta[:,1] )
+            f.close()
+            exit("The thetafile is written, you need to use predict.py now.")
+        except Exception:
+            exit("Can't open thetafile.")
+        return
+
+def train(training):
+    return
 
 
 def open_thetafile(thetafile):
@@ -32,7 +54,7 @@ def open_thetafile(thetafile):
         f.write("theta0,theta1\n0,0")
         f.close()
     except Exception:
-        exit("Can't open thetafile")
+        exit("Can't open thetafile.")
     return thetafile
 
 
@@ -58,4 +80,5 @@ if __name__ == "__main__":
     parser.add_argument("-v", "--visual", action="store_true", default=False, help="show regression")
     args = parser.parse_args()
     training = Process(args.datafile_train, args.output, rate=args.rate, range=args.range)
+    train(training)
     print(args.datafile_train)

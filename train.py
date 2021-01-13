766e23e26c1f2029e7ecefa2e68bb02afe6f6e32
maginist <maginist@student.42.fr>
2021-01-13 13:11:34 +0100

    Fix theta + ajout show regression

--- train.py
+++ train.py
@@ -32,27 +32,27 @@ class Process(object):
     def mean_square_error(self):
         return
 
-    def gradient_descent(self):
-        return
-
-    def write_theta(self):
+    def write_theta(self, t0, t1):
         try:
             f = open(t.output, "w")
-            f.write(f"theta0,theta1\n%d,%d"% t.theta[0], t.theta[1])
+            print(t0,t1)
+            f.write(f"theta0,theta1\n{t0},{t1}")
             f.close()
-            exit("The thetafile is written, you need to use predict.py now.")
+            print("The thetafile is written, you need to use predict.py now.")
         except Exception as error:
             exit(f"{error}: Can't open thetafile.")
         return
 
 def train(t):
+    m = len(t.km)
     for i in range(t.range):
-        tmp0 = (t.rate * (1/len(t.km)) * sum([t.predict(t.theta[0], t.theta[1], t.km[i]) - t.price[i] for i in range(len(t.km))]))
-        tmp1 = (t.rate * (1/len(t.km)) * sum([t.predict(t.theta[0], t.theta[1], t.km[i]- t.price[i]) * t.km[i] for i in range(len(t.km))]))
+        tmp0 = t.rate * (1 / m) * sum([t.predict(t.theta[0], t.theta[1], t.km[i]) - t.price[i] for i in range(m)])
+        tmp1 = t.rate * (1 / m) * sum([(t.predict(t.theta[0], t.theta[1], t.km[i]) - t.price[i]) * t.km[i] for i in range(m)])
         t.theta[0] -= tmp0
         t.theta[1] -= tmp1
         t.th_history[0].append(t.theta[0])
         t.th_history[1].append(t.theta[1])
+    t.write_theta(t.theta[0], t.theta[1])
     return
 
 
@@ -62,7 +62,7 @@ def open_thetafile(thetafile):
         f.write("theta0,theta1\n0,0")
         f.close()
     except Exception as error:
-            exit(f"{error}: Can't open thetafile.")
+        exit(f"{error}: Can't open thetafile.")
     return thetafile
 
 
@@ -74,18 +74,33 @@ def open_datafile(datafile):
         exit("Empty data file.")
     except pd.errors.ParserError:
         raise argparse.ArgumentTypeError("Error parsing file, needs to be a well formated csv.")
-    except Exception:
-        exit("File {datafile} corrupted or does not exist.")
+    except Exception as error:
+        exit(f"{error}: File {datafile} corrupted or does not exist.")
     return data
+
+
+def print_result(km, price, th0, th1):
+    plt.plot(km, price, "bo", label="Data")
+    plt.plot(km, th0 + th1 * km, "-r", label="regression")
+    plt.xlabel("km")
+    plt.ylabel("price")
+    plt.xlim(min(km), max(km))
+    plt.ylim(min(price), max(price))
+    plt.legend()
+    plt.title("ft_linear_regression")
+    plt.show()
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Linear regression t program")
     parser.add_argument("datafile_train", type=open_datafile, help="input a csv file well formated")
     parser.add_argument("-o", "--output", type=open_thetafile, default="theta.csv", help="output data file")
-    parser.add_argument("-r", "--range", type=int, default=1000, help="t range")
-    parser.add_argument("-rt", "--rate", type=float, default=0.1, help="t rate")
-    parser.add_argument("-v", "--visual", action="store_true", default=False, help="show regression")
+    parser.add_argument("-r", "--range", type=int, default=1000, help="training range (epochs)")
+    parser.add_argument("-rt", "--rate", type=float, default=0.1, help="training rate")
+    parser.add_argument("-s", "--show", action="store_true", default=False, help="show regression")
+    parser.add_argument("-H", "--history", action="store_true", default=False, help="show history (mean square error")
     args = parser.parse_args()
     t = Process(args.datafile_train, args.output, rate=args.rate, range=args.range)
     train(t)
+    if args.show:
+        print_result(t.km, t.price, t.theta[0], t.theta[1])
